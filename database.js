1177b67f6f3e0350790bd75813c680fe3f282135
abith <abith6495@gmail.com>
2026-06-14 00:27:43 +0530

    first commit

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
+const mysql = require('mysql2');
 
-
-// database.js
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,'Abithkani@1826' ,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'college_attendance_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Connection Test
+const promisePool = pool.promise();
+
 pool.getConnection((err, connection) => {
     if (err) {
         console.error('❌ Database Connection Failed:', err.message);
